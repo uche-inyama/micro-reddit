36f671cd2183f1ad9a13b8b2b527910b7d9c8cf0
Moses Ogwo <mosesogwo@gmail.com>
2019-08-06 17:32:11 +0100

    add user to post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+  VALID_URL_REGEX = /(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(\/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?/
+  validates :link, presence: true,
+                    format: { with: VALID_URL_REGEX }
+                    
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
 validates :password, presence: true, length: { minimum:6 }
+
+has_many :posts
 end
