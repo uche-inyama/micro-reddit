264c85777ed9909ab14f4f6cbbdf251c17f946c7
Moses Ogwo <mosesogwo@gmail.com>
2019-08-06 16:49:55 +0100

    add Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_151123) do
+ActiveRecord::Schema.define(version: 2019_08_06_154919) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "link"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
