49f9a5dee0a97c35efd12c1f73726d68a9b897b2
Moses Ogwo <mosesogwo@gmail.com>
2019-08-14 15:59:48 +0100

    correct under-indentation in user.rb, add uniqueness validation to link in post.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false }
 
-validates :password, presence: true, length: { minimum:6 }
+  validates :password, presence: true, length: { minimum:6 }
 
-has_many :posts
-has_many :comments
+  has_many :posts
+  has_many :comments
 end
