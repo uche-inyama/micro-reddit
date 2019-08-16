a5be96afca8295a6404f95bfebd56c6ade6136c2
Moses Ogwo <mosesogwo@gmail.com>
2019-08-16 11:43:25 +0100

    add index to users on email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_164436) do
+ActiveRecord::Schema.define(version: 2019_08_16_103234) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_164436) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
